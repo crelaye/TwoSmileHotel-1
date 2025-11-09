9f5bcf83ebe9ddf092b87d3beff42e6db03ada33
crelaye <152194213+crelaye@users.noreply.github.com>
2025-11-09 05:31:16 +0100

    Update booking.js

--- static/booking.js
+++ static/booking.js
@@ -119,7 +119,7 @@ document.addEventListener('DOMContentLoaded',async()=>{
                     return console.error("Error sending number of days to backend",lerr.statusText);
                  }
                  const ler= await lerr.json();
-                 window.location.href='https://twosmilehotel.onrender.com/pay.html';
+                 window.location.href=ler.success;
                  localStorage.setItem(ler.tiim,'greatDay');
             },6000)
         }
@@ -298,4 +298,5 @@ nttm.addEventListener('click',()=>{
    }
 
 })
+
 
